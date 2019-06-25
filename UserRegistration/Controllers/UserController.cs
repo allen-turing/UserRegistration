432178961d9e1e3e3e12af53e2754787cec665d7
allen-turing <prtsh004@gmail.com>
2019-06-26 04:49:37 +0530

    New Changese

---
+++ UserRegistration/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id=0)
+        {
+            User userModel = new User();    
+            return View(userModel);
+        }
+    }
+}
