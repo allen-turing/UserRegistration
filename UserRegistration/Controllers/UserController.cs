888b0f8a7cc3886a0340db42bf323d1fe4af4da4
allen-turing <prtsh004@gmail.com>
2019-06-26 05:42:29 +0530

    Form Validation Added

--- UserRegistration/Controllers/UserController.cs
+++ UserRegistration/Controllers/UserController.cs
@@ -9,10 +9,30 @@ namespace UserRegistration.Controllers
     public class UserController : Controller
     {
         // GET: User
+        [HttpGet]
         public ActionResult AddOrEdit(int id=0)
         {
             User userModel = new User();    
             return View(userModel);
+        }
+
+        // Post: User
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DbbModels dbModel = new DbbModels())
+            {
+                if (dbModel.Users.Any(x => x.Username == userModel.Username))
+                {
+                    ViewBag.DuplicateMessage = "User Name Already Exist";
+                    return View("AddOrEdit", userModel);
+                }
+                dbModel.Users.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful.";
+            return View("AddOrEdit", new User()); 
         }
     }
 }
